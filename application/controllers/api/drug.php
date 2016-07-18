39f8c198775ca741f1184cefc7c7efa7ab7197a9
Melania Laniwati <melanialani@gmail.com>
2016-07-18 23:08:25 +0700

    added limit user request

--- application/controllers/api/drug.php
+++ application/controllers/api/drug.php
@@ -4,78 +4,149 @@ require APPPATH.'/libraries/REST_Controller.php';
 
 class Drug extends REST_Controller {
 
+	function tes(){
+		$user_info = $this->Drugs_model->getUserInfo($this->get('key'));
+		$qwe = $user_info[0]['r_limit'];
+		echo($qwe);
+	}
+
 	function ndc_get(){
 		if(!$this->get('code')){
 			$this->response(array( 'status' => "Invalid FDA NDC Code" ), 400);
-		} else {
+		}
+		elseif (!$this->get('key')) {
+			$this->response(array( 'status' => "Invalid User Key" ), 400);
+		}
+		else {
 			$this->load->model('Drugs_model');
+			$user_info = $this->Drugs_model->getUserInfo($this->get('key'));
 			$drug_info = $this->Drugs_model->getDrugsWithFdaNdcCodeLike($this->get('code'));
-			
-			if(!empty($drug_info)){
-				$this->response($drug_info, 200);
-			} else {
-				$this->response(array( 'status' => "Invalid FDA NDC Code" ), 406); // need to change response formate
+			$get_request = $user_info[0]['request'];
+			$get_limit = $user_info[0]['r_limit'];
+			if($get_request < $get_limit){
+				if(!empty($drug_info)){
+					$data['request'] = $get_request + 1;
+					$this->Drugs_model->updateUserRequest($this->get('key'), $data);
+					$this->response($drug_info, 200);
+				} else {
+					$this->response(array( 'status' => "Invalid FDA NDC Code" ), 406); // need to change response formate
+				}
 			}
-		}		
+			else{
+				$this->response(array( 'status' => "Invalid Request Limit" ), 400);
+			}
+		}
 	}
-	
+
 	function diagnosis_get(){
 		if(!$this->get('code')){
 			$this->response(array( 'status' => "Invalid ICD9 Diagnosis Code" ), 400);
-		} else {
+		}
+		elseif (!$this->get('key')) {
+			$this->response(array( 'status' => "Invalid User Key" ), 400);
+		}
+		else {
 			$this->load->model('Drugs_model');
+			$user_info = $this->Drugs_model->getUserInfo($this->get('key'));
 			$drug_info = $this->Drugs_model->getDrugsWithDiagnosisCodeLike($this->get('code'));
-			
-			if(!empty($drug_info)){
-				$this->response($drug_info, 200);
-			} else {
-				$this->response(array( 'status' => "Invalid ICD9 Diagnosis Code" ), 406); // need to change response formate
+			$get_request = $user_info[0]['request'];
+			$get_limit = $user_info[0]['r_limit'];
+			if($get_request < $get_limit){
+				if(!empty($drug_info)){
+					$data['request'] = $get_request + 1;
+					$this->Drugs_model->updateUserRequest($this->get('key'), $data);
+					$this->response($drug_info, 200);
+				} else {
+					$this->response(array( 'status' => "Invalid ICD9 Diagnosis Code" ), 406); // need to change response formate
+				}
 			}
-		}		
+			else{
+				$this->response(array( 'status' => "Invalid Request Limit" ), 400);
+			}
+		}
 	}
-	
+
 	function procedure_get(){
 		if(!$this->get('code')){
 			$this->response(array( 'status' => "Invalid Procedure Code" ), 400);
-		} else {
+		}
+		elseif (!$this->get('key')) {
+			$this->response(array( 'status' => "Invalid User Key" ), 400);
+		}
+		else {
 			$this->load->model('Drugs_model');
+			$user_info = $this->Drugs_model->getUserInfo($this->get('key'));
 			$drug_info = $this->Drugs_model->getDrugsWithProcedureCodeLike($this->get('code'));
-			
-			if(!empty($drug_info)){
-				$this->response($drug_info, 200);
-			} else {
-				$this->response(array( 'status' => "Invalid Procedure Code" ), 406); // need to change response formate
+			$get_request = $user_info[0]['request'];
+			$get_limit = $user_info[0]['r_limit'];
+			if($get_request < $get_limit){
+				if(!empty($drug_info)){
+					$data['request'] = $get_request + 1;
+					$this->Drugs_model->updateUserRequest($this->get('key'), $data);
+					$this->response($drug_info, 200);
+				} else {
+					$this->response(array( 'status' => "Invalid Procedure Code" ), 406); // need to change response formate
+				}
 			}
-		}		
+			else{
+				$this->response(array( 'status' => "Invalid Request Limit" ), 400);
+			}
+		}
 	}
-	
+
 	function brand_get(){
 		if(!$this->get('name')){ // if param null
 			$this->response(array( 'status' => "Invalid Brand Name" ), 400);
-		} else {
+		}
+		elseif (!$this->get('key')) {
+			$this->response(array( 'status' => "Invalid User Key" ), 400);
+		}
+		else {
 			$this->load->model('Drugs_model');
+			$user_info = $this->Drugs_model->getUserInfo($this->get('key'));
 			$drug_info = $this->Drugs_model->getDrugsWithBrandLike($this->get('name'));
-			
-			if(!empty($drug_info)){
-				$this->response($drug_info, 200);
-			} else { // if results not found
-				$this->response(array( 'status' => "Brand Name Not Found" ), 406); // need to change response formate
+			$get_request = $user_info[0]['request'];
+			$get_limit = $user_info[0]['r_limit'];
+			if($get_request < $get_limit){
+				if(!empty($drug_info)){
+					$data['request'] = $get_request + 1;
+					$this->Drugs_model->updateUserRequest($this->get('key'), $data);
+					$this->response($drug_info, 200);
+				} else {
+					$this->response(array( 'status' => "Brand Name Not Found" ), 406); // need to change response formate
+				}
 			}
-		}		
+			else{
+				$this->response(array( 'status' => "Invalid Request Limit" ), 400);
+			}
+		}
 	}
-	
+
 	function generic_get(){
 		if(!$this->get('name')){
 			$this->response(array( 'status' => "Invalid Generic Name" ), 400);
-		} else {
+		}
+		elseif (!$this->get('key')) {
+			$this->response(array( 'status' => "Invalid User Key" ), 400);
+		}
+		else {
 			$this->load->model('Drugs_model');
+			$user_info = $this->Drugs_model->getUserInfo($this->get('key'));
 			$drug_info = $this->Drugs_model->getDrugsWithGenericNameLike($this->get('name'));
-			
-			if(!empty($drug_info)){
-				$this->response($drug_info, 200);
-			} else {
-				$this->response(array( 'status' => "Generic Name Not Found" ), 406); // need to change response formate
+			$get_request = $user_info[0]['request'];
+			$get_limit = $user_info[0]['r_limit'];
+			if($get_request < $get_limit){
+				if(!empty($drug_info)){
+					$data['request'] = $get_request + 1;
+					$this->Drugs_model->updateUserRequest($this->get('key'), $data);
+					$this->response($drug_info, 200);
+				} else {
+					$this->response(array( 'status' => "Generic Name Not Found" ), 406); // need to change response formate
+				}
 			}
-		}		
+			else{
+				$this->response(array( 'status' => "Invalid Request Limit" ), 400);
+			}
+		}
 	}
 }
--- readme.rst
+++ readme.rst
@@ -20,18 +20,18 @@ Just copy and paste it into your htdocs folder of xampp and dump the database.
 Example of XML Request
 *********
 
--  `http://localhost/_dailymed/index.php/api/drug/ndc?code=59535-3001 <http://localhost/_dailymed/index.php/api/drug/ndc?code=59535-3001>`_
--  `http://localhost/_dailymed/index.php/api/drug/diagnosis?code=20035 <http://localhost/_dailymed/index.php/api/drug/diagnosis?code=20035>`_
--  `http://localhost/_dailymed/index.php/api/drug/procedure?code=8093 <http://localhost/_dailymed/index.php/api/drug/procedure?code=8093>`_
--  `http://localhost/_dailymed/index.php/api/drug/brand?name=lam <http://localhost/_dailymed/index.php/api/drug/brand?name=lam>`_
--  `http://localhost/_dailymed/index.php/api/drug/generic?name=Midazolam <http://localhost/_dailymed/index.php/api/drug/generic?name=Midazolam>`_
+-  `http://localhost/_dailymed/index.php/api/drug/ndc?code=59535-3001&key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/ndc?code=59535-3001&key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/diagnosis?code=20035&key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/diagnosis?code=20035&key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/procedure?code=8093&key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/procedure?code=8093&key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/brand?name=lam&key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/brand?name=lam&key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/generic?name=Midazolam&key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/generic?name=Midazolam&key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
 
 *********
 Example of JSON Request
 *********
 
--  `http://localhost/_dailymed/index.php/api/drug/ndc/code/59535-3001/format/json <http://localhost/_dailymed/index.php/api/drug/ndc/code/59535-3001/format/json>`_
--  `http://localhost/_dailymed/index.php/api/drug/diagnosis/code/20035/format/json <http://localhost/_dailymed/index.php/api/drug/diagnosis/code/20035/format/json>`_
--  `http://localhost/_dailymed/index.php/api/drug/procedure/code/8093/format/json <http://localhost/_dailymed/index.php/api/drug/procedure/code/8093/format/json>`_
--  `http://localhost/_dailymed/index.php/api/drug/brand/name/lam/format/json <http://localhost/_dailymed/index.php/api/drug/brand/name/lam/format/json>`_
--  `http://localhost/_dailymed/index.php/api/drug/generic/name/Midazolam/format/json <http://localhost/_dailymed/index.php/api/drug/generic/name/Midazolam/format/json>`_
+-  `http://localhost/_dailymed/index.php/api/drug/ndc/code/59535-3001/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/ndc/code/59535-3001/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/diagnosis/code/20035/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/diagnosis/code/20035/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/procedure/code/8093/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/procedure/code/8093/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/brand/name/lam/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/brand/name/lam/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
+-  `http://localhost/_dailymed/index.php/api/drug/generic/name/Midazolam/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5 <http://localhost/_dailymed/index.php/api/drug/generic/name/Midazolam/format/json?key=72ee9a770f2ff9747dc3b8de9a73e9a5>`_
